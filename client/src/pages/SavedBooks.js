4ca9914834f060ff5749c84cbc528c3f1bf08418
Brett Smith <brettsmith.rbc2020@gmail.com>
2020-05-13 23:39:19 -0400

    readme

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -2,26 +2,18 @@ import React, { useContext } from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
 
 // import context for global state
-import UserInfoContext from '../utils/UserInfoContext';
+import SavedBookContext from '../utils/SavedBookContext';
 
+// import getSavedBooks and deleteBook from API file
 import * as API from '../utils/API';
-import AuthService from '../utils/auth';
 
 function SavedBooks() {
-  // get whole userData state object from App.js
-  const userData = useContext(UserInfoContext);
+  const { books: savedBooks, getSavedBooks } = useContext(SavedBookContext);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
-  const handleDeleteBook = (bookId) => {
-    // get token
-    const token = AuthService.loggedIn() ? AuthService.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-    API.deleteBook(bookId, token)
-      // upon succes, update user data to reflect book change
-      .then(() => userData.getUserData())
+  const handleDeleteBook = (mongoId) => {
+    API.deleteBook(mongoId)
+      .then(() => getSavedBooks())
       .catch((err) => console.log(err));
   };
 
@@ -32,22 +24,18 @@ function SavedBooks() {
           <h1>Viewing saved books!</h1>
         </Container>
       </Jumbotron>
-      <Container>
-        <h2>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'}
-        </h2>
+      <Container fluid>
+        <h2>{savedBooks.length ? `Viewing ${savedBooks.length} saved books:` : 'You have no saved books!'}</h2>
         <CardColumns>
-          {userData.savedBooks.map((book) => {
+          {savedBooks.map((book) => {
             return (
-              <Card key={book.bookId} border='dark'>
+              <Card key={book._id} border='dark'>
                 {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
                   <p className='small'>Authors: {book.authors}</p>
                   <Card.Text>{book.description}</Card.Text>
-                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book._id)}>
                     Delete this Book!
                   </Button>
                 </Card.Body>
