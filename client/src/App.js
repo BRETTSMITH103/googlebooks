c109b9720b3c85bfdb6b0b8348dda745855032b4
Brett Smith <brettsmith.rbc2020@gmail.com>
2020-05-14 00:39:55 -0400

    auth file

--- client/src/App.js
+++ client/src/App.js
@@ -5,51 +5,32 @@ import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 import * as API from './utils/API';
-import AuthService from './utils/auth';
 
-// import our context object for state
-import UserInfoContext from './utils/UserInfoContext';
+import SavedBookContext from './utils/SavedBookContext';
 
 function App() {
-  // set data to be used for UserInfoContext and make it available to all other components
-  const [userInfo, setUserInfo] = useState({
-    savedBooks: [],
-    username: '',
-    email: '',
-    bookCount: 0,
-    // method to get user data after logging in
-    getUserData: () => {
-      // if user's logged in get the token or return null
-      const token = AuthService.loggedIn() ? AuthService.getToken() : null;
 
-      if (!token) {
-        return false;
-      }
-      API.getMe(token)
-        .then(({ data: { username, email, savedBooks, bookCount } }) =>
-          setUserInfo({ ...userInfo, username, email, savedBooks, bookCount })
-        )
-        .catch((err) => console.log(err));
+  const [savedBookState, setSavedBookState] = useState({
+    books: [],
+    getSavedBooks: () => {
+      API.getSavedBooks().then(({ data }) => setSavedBookState({ ...savedBookState, books: data }));
     },
   });
-
-  // on load, get user data if a token exists
   useEffect(() => {
-    userInfo.getUserData();
-  });
+    savedBookState.getSavedBooks();
+  }, []);
 
   return (
     <Router>
       <>
-        {/* wrap our entire app in context provider and provide userInfo state as value */}
-        <UserInfoContext.Provider value={userInfo}>
-          <Navbar />
+        <Navbar />
+        <SavedBookContext.Provider value={savedBookState}>
           <Switch>
             <Route exact path='/' component={SearchBooks} />
             <Route exact path='/saved' component={SavedBooks} />
             <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
           </Switch>
-        </UserInfoContext.Provider>
+        </SavedBookContext.Provider>
       </>
     </Router>
   );
